e539b102b9c80963b75835e976e0cbf572dbdaa2
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2025-04-01 11:59:50 +0200

    <job01 - job06> terminé / Installation de devtools

---
+++ jour01/job06/runtrack/src/main/java/com/example/runtrack/RuntrackApplication.java
@@ -0,0 +1,12 @@
+package com.example.runtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntrackApplication.class, args);
+	}
+}
